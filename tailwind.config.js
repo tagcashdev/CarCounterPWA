cbb169c1ebf7d24e64a3f3bdd8ca1608e2b92e42
TagCashDev <tagcashdev@gmail.com>
2024-07-09 13:15:08 +0200

    tailly color
    add tailly color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tailly': '#f7f6fb',
+      },
+    },
   },
   plugins: [],
 }
