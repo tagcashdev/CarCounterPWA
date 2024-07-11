6ed17f0ddbc1e2fb3b26ed2195fa0ea226338328
TagCashDev <tagcashdev@gmail.com>
2024-07-11 09:29:43 +0200

    Gestion mise à jour app / cache

--- service-worker.js
+++ service-worker.js
@@ -9,31 +9,29 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+      caches.open(CACHE_NAME).then(cache => {
+          return cache.addAll(FILES_TO_CACHE);
+      }).then(() => {
+          self.skipWaiting();
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+      caches.match(event.request).then(response => {
+          return response || fetch(event.request).then(networkResponse => {
+              if (networkResponse && networkResponse.status === 200) {
+                  caches.open(CACHE_NAME).then(cache => {
+                      cache.put(event.request, networkResponse.clone());
+                  });
+              }
+              return networkResponse;
+          });
+      })
   );
 });
 
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
